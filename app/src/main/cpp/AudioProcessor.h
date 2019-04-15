470dccb6b5cdcf3fe3b1680e78d766608c62d362
Patrick Gorman <gormanpe@vcu.edu>
2019-04-15 18:37:08 -0400

    Beginning to add CPP files

---
+++ app/src/main/cpp/AudioProcessor.h
@@ -0,0 +1,11 @@
+#ifndef MICANDMASTER_AUDIOPROCESSOR_H
+#define MICANDMASTER_AUDIOPROCESSOR_H
+
+#include <jni.h>
+#include <oboe/Oboe.h>
+#include <string>
+
+
+
+
+#endif //MICANDMASTER_AUDIOPROCESSOR_H
