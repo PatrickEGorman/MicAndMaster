9b7a700156f3f97bd89505d8baeee75e44942cd8
Patrick Gorman <gormanpe@vcu.edu>
2019-04-24 16:35:07 -0400

    Removed c native lib

--- app/src/main/java/com/example/micandmaster/audio/WaveForm.java
+++ app/src/main/java/com/example/micandmaster/audio/WaveForm.java
@@ -5,15 +5,8 @@ import android.content.Context;
 public class WaveForm {
     Audio mAudio;
 
-    static {
-        System.loadLibrary("native-lib");
-    }
-
     public WaveForm(Audio audio, Context context) {
         mAudio = audio;
         mAudio.generatePath(context);
-        System.out.println(array(mAudio.path)[2]);
     }
-
-    public native int[] array(String jstring1);
 }
