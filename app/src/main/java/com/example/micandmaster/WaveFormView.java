966d5244e6b781398e6b27c5a76f3ab715e5fe13
Patrick Gorman <gormanpe@vcu.edu>
2019-04-25 21:46:19 -0400

    added line preview to waveform view

--- app/src/main/java/com/example/micandmaster/WaveFormView.java
+++ app/src/main/java/com/example/micandmaster/WaveFormView.java
@@ -12,24 +12,46 @@ import android.view.View;
 
 
 public class WaveFormView extends View {
-    private Paint paint = new Paint();
+    private Paint paint;
 
     public WaveFormView(Context context) {
         super(context);
+        init();
     }
 
     public WaveFormView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public WaveFormView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        init();
+    }
+
+    private void init(){
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        paint.setColor(0xff101010);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawLine(200, 200, 300, 300, paint);
+    }
 
-        paint.setARGB(100, 100, 100, 100);
-        canvas.drawLine(0, 0, 100, 100, paint);
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+//        // Try for a width based on our minimum
+//        int minw = getPaddingLeft() + getPaddingRight() + getSuggestedMinimumWidth();
+//        int w = resolveSizeAndState(minw, widthMeasureSpec, 1);
+//
+//        // Whatever the width ends up being, ask for a height that would let the pie
+//        // get as big as it can
+//        int minh = MeasureSpec.getSize(w) - (int)mTextWidth + getPaddingBottom() + getPaddingTop();
+//        int h = resolveSizeAndState(MeasureSpec.getSize(w) - (int)mTextWidth, heightMeasureSpec, 0);
+
+        setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
     }
 }
